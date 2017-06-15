529bdbc1ceef34dfe14f2c77f6165ebd1a27710b
jimmy820904 <jimmy820904@gmail.com>
2017-06-15 15:25:50 +0800

    test

---
+++ git/SQA/src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by acer on 2017/6/15.
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("123");
+        System.out.println("456");
+    }
+}
