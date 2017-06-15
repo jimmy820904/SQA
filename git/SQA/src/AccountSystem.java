7371ecd41140bad582d13f5cc705a483f972526c
jimmy820904 <jimmy820904@gmail.com>
2017-06-15 16:28:36 +0800

    測試檢查字元

---
+++ git/SQA/src/AccountSystem.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+/**
+ * Created by acer on 2017/6/15.
+ */
+public class AccountSystem
+{
+
+}
--- git/SQA/src/Test.java
+++ git/SQA/src/Test.java
@@ -5,8 +5,18 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("123");
-        System.out.println("456");
-        System.out.print("finish");
+        String s = "1Ba";
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            char temp = s.charAt(i);
+            if( Character.isUpperCase(temp) )
+                System.out.print("大寫");
+            if( Character.isLowerCase(temp) )
+                System.out.print("小寫");
+            if( Character.isDigit(temp) )
+                System.out.print("數字");
+            System.out.println();
+        }
     }
 }
